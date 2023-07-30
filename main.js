160104dacfb51fc1185a7e550b7ac9718ccd4343
Sayandip Dutta <133519673+SayandipDutta23@users.noreply.github.com>
2023-07-30 12:00:10 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -25,7 +25,9 @@ const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
 const material = new THREE.MeshStandardMaterial({ color: 0xff6347 });
 const torus = new THREE.Mesh(geometry, material);
 
+
 scene.add(torus);
+
 
 // Lights
 
@@ -60,12 +62,12 @@ Array(200).fill().forEach(addStar);
 
 // Background
 
-const spaceTexture = new THREE.TextureLoader().load('space.jpg');
+const spaceTexture = new THREE.TextureLoader().load('space.webp');
 scene.background = spaceTexture;
 
 // Avatar
 
-const jeffTexture = new THREE.TextureLoader().load('jeff.png');
+const jeffTexture = new THREE.TextureLoader().load('jeff.webp');
 
 const jeff = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3), new THREE.MeshBasicMaterial({ map: jeffTexture }));
 
@@ -73,8 +75,8 @@ scene.add(jeff);
 
 // Moon
 
-const moonTexture = new THREE.TextureLoader().load('moon.jpg');
-const normalTexture = new THREE.TextureLoader().load('normal.jpg');
+const moonTexture = new THREE.TextureLoader().load('moon.webp');
+const normalTexture = new THREE.TextureLoader().load('normal.webp');
 
 const moon = new THREE.Mesh(
   new THREE.SphereGeometry(3, 32, 32),
@@ -128,3 +130,4 @@ function animate() {
 }
 
 animate();
+
